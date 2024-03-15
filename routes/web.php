6438a16d16dff8bedd250d27df1aa3d865751166
FatmaElzahraa227 <70411867+FatmaElzahraa227@users.noreply.github.com>
2024-03-15 15:57:18 +0200

    updated

--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\TestController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// use App\Http\Controllers\TestController;
 
 // Route::get('/posts',"PostController@index");
 
 // Route::get('/tests',[TestController::class,'index']);
 
-Route::get('/posts',[PostController::class,'index'])->name('posts.index');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
-Route::post('/posts',[PostController::class,'store'])->name('posts.store');
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
-Route::get('/posts/{id}', [PostController::class,'show'])->where('id', '[0-9]+')->name('posts.show');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 
-Route::get('/posts/{id}/edit',[PostController::class,'edit'])->where('id', '[0-9]+')->name('posts.edit');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
-Route::put('/posts/{id}',[PostController::class,'update'])->where('id', '[0-9]+')->name('posts.update');
+Route::get('/posts/{id}', [PostController::class, 'show'])->where('id', '[0-9]+')->name('posts.show');
 
-Route::delete('/posts/{id}',[PostController::class,'destroy'])->where('id', '[0-9]+')->name('posts.destroy');
+Route::get('/posts/{id}/edit', [PostController::class, 'edit'])->where('id', '[0-9]+')->name('posts.edit');
+
+Route::put('/posts/{id}', [PostController::class, 'update'])->where('id', '[0-9]+')->name('posts.update');
+
+Route::delete('/posts/{id}', [PostController::class, 'destroy'])->where('id', '[0-9]+')->name('posts.destroy');
+
+Route::get('/posts/trash', [PostController::class, 'trash'])->name('posts.trash');
 
 
 
-Route::fallback(fn()=>"Route Not Found");
+Route::fallback(fn () => "Route Not Found");
